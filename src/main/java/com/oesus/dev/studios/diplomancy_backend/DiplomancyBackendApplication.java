d8ca5161bab3f222be331a8ecf31e2667b55c2d6
Mqwere <patapatapatapon.chakachakapata@gmail.com>
2025-11-29 20:28:51 +0100

    init

---
+++ src/main/java/com/oesus/dev/studios/diplomancy_backend/DiplomancyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.oesus.dev.studios.diplomancy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomancyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomancyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oesus/dev/studios/diplomancy_backend/DiplomancyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oesus.dev.studios.diplomancy_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomancyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
