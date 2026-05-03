49fda7e3fedd84cc5a20299bc3d57097926e268c
Adam Krizar <adam.krizar@gmail.com>
2026-05-03 11:25:24 +0200

    Develop backend

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import request, Flask
 
 app = Flask("diplomancy-backend")
 
-@app.route("/logon", methods=["POST"])
-def logon():
+@app.route("/api/v1/auth/logon/<uname>", methods=["POST"])
+def logon(uname):
+    print(uname)
+    print(request.get_json())
     return "Logon endpoint"
 
 def execute():
