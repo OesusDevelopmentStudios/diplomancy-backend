0f79dbbe3aa81c1ad5670fbd1de878e60ca7c25f
Adam Krizar <adam.krizar@gmail.com>
2026-05-03 15:46:41 +0200

    1. CORS handling 2. Update logon api

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from flask import request, Flask
+from flask_cors import CORS
 
 app = Flask("diplomancy-backend")
+CORS(app)
 
-@app.route("/api/v1/auth/logon/<uname>", methods=["POST"])
-def logon(uname):
-    print(uname)
+@app.route("/api/v1/auth/logon", methods=["POST"])
+def logon():
+    print("Received logon request")
     print(request.get_json())
     return "Logon endpoint"
 
